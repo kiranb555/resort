e54eb0919caa4833b9b1fe7bf6e48637851c9574
kiranb555 <kiran.basavaraj05@gmail.com>
2019-09-16 00:38:22 +0530

    added Route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import Home from './pages/Home';
-import Rooms from './pages/Rooms';
-import SingleRoom from './pages/SingleRoom';
-import Error from './pages/ErrorPage';
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/ErrorPage";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      <Rooms/>
-      <SingleRoom/>
-      <Error/>
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
